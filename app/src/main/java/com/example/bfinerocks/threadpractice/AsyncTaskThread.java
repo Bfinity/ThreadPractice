ca9f788e2dc49deca0a0d2cf95e4b608267612fd
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-15 12:27:54 -0500

    created a class that extends async task

---
+++ app/src/main/java/com/example/bfinerocks/threadpractice/AsyncTaskThread.java
@@ -0,0 +1,9 @@
+package com.example.bfinerocks.threadpractice;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by BFineRocks on 12/15/14.
+ */
+public class AsyncTaskThread extends AsyncTask<Void, Void, Void>{
+}
