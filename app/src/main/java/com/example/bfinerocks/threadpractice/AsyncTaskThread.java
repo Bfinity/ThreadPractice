bc121b8a9e3baa54066af2eae57d05a0ba8a76c2
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-15 12:29:30 -0500

    implemented do in background method, created string with value of thread, created log message

--- app/src/main/java/com/example/bfinerocks/threadpractice/AsyncTaskThread.java
+++ app/src/main/java/com/example/bfinerocks/threadpractice/AsyncTaskThread.java
@@ -1,9 +1,16 @@
 package com.example.bfinerocks.threadpractice;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 /**
  * Created by BFineRocks on 12/15/14.
  */
 public class AsyncTaskThread extends AsyncTask<Void, Void, Void>{
+    @Override
+    protected Void doInBackground(Void... voids) {
+        String thread = String.valueOf(Thread.currentThread().getId());
+        Log.i("AsyncTaskThread", thread);
+        return null;
+    }
 }
