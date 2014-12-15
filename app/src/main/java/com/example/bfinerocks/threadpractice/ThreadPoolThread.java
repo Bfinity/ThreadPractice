1fd3df70678b0dcb2296409fe720f3c5cd44c3cb
Brandy Foster <brandy.foster@detroitlabs.com>
2014-12-15 16:01:03 -0500

    created a linkedblockingqueue

--- app/src/main/java/com/example/bfinerocks/threadpractice/ThreadPoolThread.java
+++ app/src/main/java/com/example/bfinerocks/threadpractice/ThreadPoolThread.java
@@ -1,14 +1,22 @@
 package com.example.bfinerocks.threadpractice;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by BFineRocks on 12/15/14.
  */
 public class ThreadPoolThread{
 
     public static ThreadPoolThread sThreadPoolThread;
+    private static int NUM_OF_CORES = Runtime.getRuntime().availableProcessors();
+    private static final int KEEP_ALIVE_TIME = 1;
+    private static final TimeUnit KEEP_ALIVE_TIME_UNIT = TimeUnit.SECONDS;
+    private final BlockingQueue<Runnable> queue;
 
     private ThreadPoolThread(){
-
+        queue = new LinkedBlockingDeque<Runnable>();
     }
 
     public static ThreadPoolThread getThreadPoolThread(){
@@ -17,6 +25,8 @@ public class ThreadPoolThread{
         }
         return sThreadPoolThread;
     }
+
+
 
 
 }
